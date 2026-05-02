9f715e5375a460651d96fd316b57c5a0c7461efd
AT6 <Dantedowhy@icloud.com>
2026-05-02 16:48:18 -0500

    Implement loading screen with delay
    Add loading screen functionality to display app after delay.

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.getElementById("loading-screen").style.display = "none";
+        document.getElementById("app").style.display = "block";
+    }, 2600); // matches animation timing
+});
